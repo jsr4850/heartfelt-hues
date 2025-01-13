e547ad33b34f3b381869ec6d7611637cfbbb15cb
jsr4850 <47167930+jsr4850@users.noreply.github.com>
2025-01-13 13:18:04 -0600

    Update home.tsx

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -5,8 +5,8 @@ export default function HomePage() {
   return (
     <Layout>
       <Hero
-        title="Landing template for startups"
-        content="Our landing page template works for all the devices, so you only have to setup it once, and get beautiful results forever."
+        title="Landing Page ....."
+        content=" Building go to destination for personalised gifts"
         illustration={<HeroIllustration />}
       />
     </Layout>
