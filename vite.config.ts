74ba3936faa214fa38d2c5e3756f48b060d6772e
jsr4850 <47167930+jsr4850@users.noreply.github.com>
2025-01-13 13:37:19 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   root: 'src',
-  base: '/holly-react/',
+  base: '/heartfelthues.github.io/',
   build: {
     outDir: '../dist',
   },
